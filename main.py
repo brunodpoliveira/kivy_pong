9527b6007c647d0af7a7d4dcd10e1805d8cdb956
Bruno Daniel <70975013+brunodpoliveira@users.noreply.github.com>
2021-08-21 11:50:55 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+
+kivy.require('2.0.0')  # replace with your current kivy version !
+
+
+class MyApp(App):
+
+    def build(self):
+        return Label(text='Hello world')
+
+
+if __name__ == '__main__':
+    MyApp().run()
